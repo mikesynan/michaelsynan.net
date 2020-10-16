0ca153f28fc4c87deb191e29f01cefa2277ff69c
git <contact@openwavedigital.com>
2020-10-15 23:18:15 -0400

    everything

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,25 +1,10 @@
 <template>
-
   <nav class="max-w-6xl mx-auto flex items-center justify-between flex-wrap p-4 sticky top-0">
     <div class="flex-1 inline-flex lg:mr-6 md:mr-6 mr-0 sm:mr-0">
-      <a class="no-underline hover:no-underline" href="#">
+    <nuxt-link to="/">
         <img src="~static/test.jpeg" alt="" style="max-height: 50px; display: inline-block;">
-        <nuxt-link to="/"><span class="lg:text-2xl xl:text-2xl md:text-xl sm:text-base text-base pl-2 text-gray-800"><i class="em em-grinning"></i>Michael Synan</span></nuxt-link>
-      </a>
+        <span class="lg:text-2xl xl:text-2xl md:text-xl sm:text-lg text-lg pl-2 text-gray-800"><i class="em em-grinning"></i>Michael Synan</span></nuxt-link>
     </div>
-    <!--
-  <div class="block lg:invisible">
-    <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-white hover:border-white outline-none">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <title>Menu</title>
-        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-      </svg>
-    </button>
-  </div>
--->
-    <!--
-  <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content">
-  -->
     <div class="flex-end inline-flex pt-0 lg:pt-0 mx-auto" id="nav-content">
       <ul class="list-reset inline-flex">
         <li class="mr-3">
@@ -38,7 +23,6 @@
       </ul>
     </div>
   </nav>
-
 </template>
 
 <script>
@@ -52,5 +36,4 @@ export default {
 </script>
 
 <style>
-nav {}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,25 +7,22 @@
         <div>
           <section class="text-gray-700">
             <div class="container mx-auto">
-
               <div class="sm:mx-auto sm:mb-2">
                 <div class="py-2">
                   <details class="mb-4">
                     <summary class="border-solid border-4 border-gray-600 font-semibold  bg-opal rounded-md py-2 px-4 shadow cursor-pointer transition duration-150 ease-in-out">
                       Website Development and Design
                     </summary>
-
                     <div class="p-2 opacity-0 transition duration-200 ease-in-out bg-white  border-solid border-2 border-gray-600 mt-2 shadow">
-                    I do website design and development. Mostly interested in Vue,
-                    but happy building on WordPress or without a framework.
+                      I do website design and development. Mostly interested in Vue,
+                      but happy building on WordPress or without a framework.
                     </div>
                   </details>
                   <details class="mb-4">
                     <summary class="border-solid border-4 border-gray-600 font-semibold bg-opal rounded-md py-2 px-4 shadow cursor-pointer transition duration-150 ease-in-out">
                       Homelab Setup
                     </summary>
-
-                      <div class="p-2  opacity-0 transition duration-200 ease-in-out bg-white  border-solid border-2 border-gray-600 mt-2 shadow">
+                    <div class="p-2  opacity-0 transition duration-200 ease-in-out bg-white  border-solid border-2 border-gray-600 mt-2 shadow">
                       I first got into Linux in the early 2000's in response to privacy concerns about Microsoft Windows and the restrictions
                       they place on software. I wanted to be able to confirm that my Operating System wasn't spying on me, control what it's doing
                       and make it look the way I wanted. That led me to Ubuntu 12.04, since then I've expanded out to Linux Mint and then Manjaro.
@@ -35,7 +32,6 @@
                     <summary class="border-solid border-4 border-gray-600 font-semibold  bg-opal rounded-md py-2 px-4 shadow cursor-pointer transition duration-150 ease-in-out">
                       This Website
                     </summary>
-
                     <div class="p-2 opacity-0 transition duration-200 ease-in-out bg-white  border-solid border-2 border-gray-600 mt-2 shadow">
                       Built with Nuxt and Tailwind CSS, deployed via Github on Netlify.
                     </div>
@@ -52,53 +48,56 @@
           <div class="text-2xl pb-4">
             Welcome to my website, please feel free to check out my blog and other things.
           </div>
-<div class="sm:invisible lg:visible md:visible invisible">
-      <span class="p-2"> <fa :icon="faGithub"  transform="grow-8" value="27" class="cursor-pointer hover:text-darkcyan2 hover:shadow" /></span>
-      <span class="p-2" <fa :icon="faTwitter" transform="grow-8" value="27"  class="cursor-pointer hover:text-darkcyan2 hover:shadow" /></span>
-</div>
-     </div>
-
-
+          <div class="sm:invisible sm:h-0 lg:visible md:visible h-0 invisible">
+            <span class="p-2">
+              <fa :icon="faGithub" transform="grow-8" value="27" class="cursor-pointer hover:text-darkcyan2 hover:shadow" /></span>
+            <span class="p-2" <fa :icon="faTwitter" transform="grow-8" value="27" class="cursor-pointer hover:text-darkcyan2 hover:shadow" /></span>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
+  <div class="sm:visible lg:invisible md:invisible visible">
+    <span class="p-2">
+      <fa :icon="faGithub" transform="grow-8" value="27" class="cursor-pointer hover:text-darkcyan2 hover:shadow" /></span>
+    <span class="p-2" <fa :icon="faTwitter" transform="grow-8" value="27" class="cursor-pointer hover:text-darkcyan2 hover:shadow" /></span>
   </div>
 </div>
 </template>
 
 <script>
+import {
+  fas
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons'
 
+export default {
+  head: {
+    title: 'My cool home page',
+    meta: [{
+      hid: 'description',
+      name: 'description',
+      content: 'my cool home page content'
+    }],
+  },
+  computed: {
+    fas() {
+      return fas
+    },
+    faGithub() {
+      return faGithub
+    },
+    faTwitter() {
+      return faTwitter
+    },
 
-
-  import { fas } from '@fortawesome/free-solid-svg-icons'
-   import { faGithub } from '@fortawesome/free-brands-svg-icons'
-   import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-
-   export default {
-     head: {
-       title: 'My cool home page',
-       meta: [
-         {
-           hid: 'description',
-           name: 'description',
-           content: 'my cool home page content'
-         }
-       ],
-     },
-     computed: {
-       fas () {
-          return fas
-       },
-       faGithub () {
-          return faGithub
-       },
-       faTwitter () {
-          return faTwitter
-       },
-
-     },
-   }
-
-
+  },
+}
 </script>
 
 <style>
@@ -120,7 +119,13 @@
   text-align: center;
 }
 
-details[open=""] summary {background-color: darkcyan !important; color: white !important; border-color: black !important;}
-details[open=""] div {opacity: 1 !important;}
+details[open=""] summary {
+  background-color: darkcyan !important;
+  color: white !important;
+  border-color: black !important;
+}
 
+details[open=""] div {
+  opacity: 1 !important;
+}
 </style>
